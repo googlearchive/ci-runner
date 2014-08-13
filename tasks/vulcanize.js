4c37329cb0fa366927a4084d59d7d91b7dc5e879
Ian MacLeod <imac@google.com>
2014-08-12 20:47:28 -0700

    bundle platform and friends, too.

--- tasks/vulcanize.js
+++ tasks/vulcanize.js
@@ -4,8 +4,9 @@ module.exports = function vulcanize(grunt) {
     vulcanize: {
       default: {
         options: {
-          csp: true,
-          strip: true,
+          csp:    true,
+          inline: true,
+          strip:  true,
         },
         files: {
           'gh-pages/index.html': 'index.html',

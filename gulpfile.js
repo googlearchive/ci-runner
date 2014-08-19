62074569e7b733337276f20c1f8e55f8c940a38d
Ian MacLeod <imac@google.com>
2014-08-19 10:20:35 -0700

    Gulp.
    Not really piping anything yet; but it's simple for now.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp  = require('gulp');
+var shell = require('gulp-shell');
+
+gulp.task('bower', function() {
+  return require('gulp-bower')('client/components');
+});
+
+gulp.task('vulcanize', ['bower'], function() {
+  return gulp.src('client/index.html')
+      .pipe(require('gulp-vulcanize')({
+        dest:   'gh-pages',
+        csp:    true,
+        inline: true,
+        strip:  true,
+      }))
+      .pipe(gulp.dest('gh-pages'));
+});
+
+gulp.task('publish', ['vulcanize'], shell.task([
+  'cd gh-pages && git commit --all -m "Published via \\`bower publish\\`." && git push origin gh-pages',
+  'git commit --all -m "Published via \\`bower publish\\`." && git push origin gh-pages',
+]));
+
+gulp.task
